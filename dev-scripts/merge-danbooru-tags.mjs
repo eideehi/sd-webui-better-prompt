d7f3439d7c7365b2c0dbbbf49cf8ff1e38c9259e
eideehi <41231321+eideehi@users.noreply.github.com>
2023-06-06 20:33:34 +0900

    Modify the Danbooru tags merge script

--- dev-scripts/merge-danbooru-tags.mjs
+++ dev-scripts/merge-danbooru-tags.mjs
@@ -5,21 +5,27 @@ const __dirname = path.resolve();
 
 const dir = path.join(__dirname, "tmp");
 
+const omit = ({ name, post_count, category }) => {
+  return { name, post_count, category };
+};
+
 fs.readdir(dir).then((files) => {
   const jsonFiles = files
     .filter((file) => file.endsWith(".json"))
+    .sort()
     .map((file) => path.resolve(dir, file));
 
   Promise.all(jsonFiles.map((pathname) => fs.readJson(pathname))).then((allTags) => {
-    let merge = [];
+    let merge = new Map();
 
     allTags.forEach((tags) => {
       if (Array.isArray(tags)) {
         const map = new Map(tags.map((tag) => [tag.name, tag]));
-        map.forEach((tag) => merge.push(tag));
+        merge = new Map([...merge, ...map]);
       }
     });
 
-    fs.writeJson(path.resolve(dir, "danbooru-tags.json"), merge);
+    const tags = Array.from(merge.values());
+    fs.writeJson(path.resolve(dir, "danbooru-tags.json"), tags.map(omit));
   });
 });
