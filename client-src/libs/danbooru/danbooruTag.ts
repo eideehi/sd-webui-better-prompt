aabb1f69baefaad712f835f68ff9c49c8b5c8688
eideehi <41231321+eideehi@users.noreply.github.com>
2023-06-06 20:42:59 +0900

    Change the type information of DanbooruTag

--- client-src/libs/danbooru/danbooruTag.ts
+++ client-src/libs/danbooru/danbooruTag.ts
@@ -2,6 +2,4 @@ export type DanbooruTag = {
   name: string;
   post_count: number;
   category: 0 | 3 | 4;
-  is_deprecated: boolean;
-  words: string[];
 };
--- client-src/libs/danbooru/isDanbooruTag.ts
+++ client-src/libs/danbooru/isDanbooruTag.ts
@@ -4,11 +4,5 @@ export function isDanbooruTag(obj: unknown): obj is DanbooruTag {
   if (obj == null || typeof obj !== "object") return false;
   if (typeof obj["name"] !== "string") return false;
   if (typeof obj["post_count"] !== "number") return false;
-  if (typeof obj["category"] !== "number" || ![0, 3, 4].includes(obj["category"])) return false;
-  if (typeof obj["is_deprecated"] !== "boolean") return false;
-  return (
-    "words" in obj &&
-    Array.isArray(obj.words) &&
-    obj.words.every((value) => typeof value === "string")
-  );
+  return typeof obj["category"] === "number" && [0, 3, 4].includes(obj["category"]);
 }
