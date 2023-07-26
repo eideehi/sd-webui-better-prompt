025b7cf69fbcdc545af24be273af606483211f51
eideehi <41231321+eideehi@users.noreply.github.com>
2023-07-26 21:01:38 +0900

    Remove unreferenced TypeScript files

--- client-src/libs/util/dom/hasClass.ts
+++
@@ -1,5 +0,0 @@
-import { omitNulls } from "@/libs/util/array";
-
-export function hasClass(element: Element, ...classes: Nullable<string>[]): boolean {
-  return omitNulls(classes).every((clazz) => element.classList.contains(clazz));
-}
--- client-src/libs/util/string/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./generateHashCode";
-export * from "./toggleValue";
